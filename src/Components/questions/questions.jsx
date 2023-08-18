d34beccbba8f74dc6ab70ea52e1fcca6df25f257
Wilson Filho <will.flho@gmail.com>
2023-08-18 00:35:04 -0300

    fix auth

--- src/Components/questions/questions.jsx
+++ src/Components/questions/questions.jsx
@@ -97,6 +97,13 @@ function Questions() {
         borderRadius: '10px',
     };
 
+    const makeRequest = async (url) => {
+        const res = await axios.post(url, {}, {
+                headers: { Authorization: `Bearer ${localStorage.getItem('user-token')}` }
+            })
+        return res;
+    }
+
 
     useEffect(() => {
         document.title = 'Tesouro Direto';
@@ -108,7 +115,7 @@ function Questions() {
 
         console.log(age);
         try {
-            const res = await axios.get(`https://${SERVERIP}/api/age-distribution?age_min=${age[0]}&age_max=${age[1]}`);
+            const res = makeRequest(`https://${SERVERIP}/api/age-distribution?age_min=${age[0]}&age_max=${age[1]}`);
             setResponse(res.data.result);
         } catch (error) {
             console.error(error);
@@ -122,7 +129,7 @@ function Questions() {
         setLoading2(true);
 
         try {
-            const res = await axios.get(`https://${SERVERIP}/api/top-n-common-jobs/`);
+            const res = makeRequest(`https://${SERVERIP}/api/top-n-common-jobs/`);
             setResponse2(res.data.result);
         } catch (error) {
             console.error(error);
@@ -136,7 +143,7 @@ function Questions() {
         setLoading3(true);
 
         try {
-            const res = await axios.get(`https://${SERVERIP}/api/active-inactive-investidors/`);
+            const res = makeRequest(`https://${SERVERIP}/api/active-inactive-investidors/`);
             setResponse3(res.data.result);
         } catch (error) {
             console.error(error);
@@ -150,7 +157,7 @@ function Questions() {
         setLoading4(true);
 
         try {
-            const res = await axios.get(`https://${SERVERIP}/api/state-most-investidors/`);
+            const res = makeRequest(`https://${SERVERIP}/api/state-most-investidors/`);
             setResponse4(res.data.result);
         } catch (error) {
             console.error(error);
@@ -164,7 +171,7 @@ function Questions() {
         setLoading5(true);
 
         try {
-            const res = await axios.get(`https://${SERVERIP}/api/city-most-investidors?state=${uf}`);
+            const res = makeRequest(`https://${SERVERIP}/api/city-most-investidors?state=${uf}`);
 
             setResponse5(res.data.result);
         } catch (error) {
@@ -179,7 +186,7 @@ function Questions() {
         setLoading6(true);
 
         try {
-            const res = await axios.get(`https://${SERVERIP}/api/civil-status-year-activity/`);
+            const res = makeRequest(`https://${SERVERIP}/api/civil-status-year-activity/`);
             setResponse6(res.data.result);
         } catch (error) {
             console.error(error);
@@ -193,7 +200,7 @@ function Questions() {
         setLoading7(true);
 
         try {
-            const res = await axios.get(`https://${SERVERIP}/api/investidors-genre?age=${ageUF}`);
+            const res = makeRequest(`https://${SERVERIP}/api/investidors-genre?age=${ageUF}`);
             setResponse7(res.data.result);
         } catch (error) {
             console.error(error);
@@ -208,7 +215,7 @@ function Questions() {
         setLoading8(true);
 
         try {
-            const res = await axios.get(`https://${SERVERIP}/api/accession-date-trend/`);
+            const res = makeRequest(`https://${SERVERIP}/api/accession-date-trend/`);
             setResponse8(res.data.result);
         } catch (error) {
             console.error(error);
@@ -222,7 +229,7 @@ function Questions() {
         setLoading9(true);
 
         try {
-            const res = await axios.get(`https://${SERVERIP}/api/most-common-jobs-year-activities/`);
+            const res = makeRequest(`https://${SERVERIP}/api/most-common-jobs-year-activities/`);
             setResponse(res.data.result);
         } catch (error) {
             console.error(error);
@@ -236,7 +243,7 @@ function Questions() {
         setLoading9(true);
 
         try {
-            const res = await axios.get(`https://${SERVERIP}/api/investidor-activity-year-carreer/`);
+            const res = makeRequest(`https://${SERVERIP}/api/investidor-activity-year-carreer/`);
             setResponse9(res.data.result);
         } catch (error) {
             console.error(error);
